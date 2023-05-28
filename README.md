d9558785164636c97f701a1e319a2b06a8725923
edna-lab <132206658+edna-lab@users.noreply.github.com>
2023-05-28 14:24:48 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Edna
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-pqkcg8)
---
+++ src/app/sharepage/sharepage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sharepage',
+  templateUrl: './sharepage.component.html',
+  styleUrls: ['./sharepage.component.css']
+})
+export class SharepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
